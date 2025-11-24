357d393f342bad2676dbd354f0f0d78f91bb17c3
Hyunju Choi <blackpost38@gmail.com>
2025-11-24 10:48:25 +0900

    configure package for npm publishing

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,36 @@
 import { fileURLToPath, URL } from 'node:url'
+import { dirname, resolve } from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VueNipplejs',
+      // the proper extensions will be added
+      fileName: 'vue-nipplejs',
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['vue', 'nipplejs'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          nipplejs: 'nipplejs',
+        },
+      },
     },
   },
 })

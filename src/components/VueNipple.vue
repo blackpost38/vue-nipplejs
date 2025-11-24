4a9a0f61a98e230143123647d45e83f4b0e7cf16
Hyunju Choi <blackpost38@gmail.com>
2025-11-24 11:30:40 +0900

    Add initial VueNipple base component

---
+++ src/components/VueNipple.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onMounted, shallowRef, useTemplateRef } from 'vue'
+import nipplejs from 'nipplejs'
+
+const manager = shallowRef<nipplejs.JoystickManager | null>(null)
+const nippleRef = useTemplateRef<HTMLDivElement>('nipple')
+
+onMounted(() => {
+  manager.value = nipplejs.create({
+    zone: nippleRef.value!,
+    color: 'blue',
+  })
+})
+</script>
+
+<template>
+  <div ref="nipple" class="nipple"></div>
+</template>
+
+<style scoped>
+.nipple {
+  position: relative;
+}
+</style>
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as VueNipple } from '@/components/VueNipple.vue'
